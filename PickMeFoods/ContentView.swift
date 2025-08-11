a09ceb27b887c983d01a774345ca8df2a98923f0
Bishan Meddegoda <bishan.meddegoda@pickme.lk>
2025-08-11 14:14:33 +0530

    Initial Commit

---
+++ PickMeFoods/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PickMeFoods
+//
+//  Created by Bishanm on 2025-08-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PickMeFoods/PickMeFoodsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickMeFoodsApp.swift
+//  PickMeFoods
+//
+//  Created by Bishanm on 2025-08-11.
+//
+
+import SwiftUI
+
+@main
+struct PickMeFoodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
